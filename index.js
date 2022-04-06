d3d31a0660f92fc76a3b4b7059353a8c5f8f5396
Harshal Carpenter <hcarpenter@tripactions.com>
2022-04-06 15:12:57 -0700

    bugfix/LQ-453 - Admin - Full feature

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const DELIMITER = '-delimiter-';
+const DELIMITER = '-~-';
 
 function onInit() {
     let logs = process.env.changelog.split(DELIMITER);
-    console.log(logs);
+}
+
+function formatMessage(logs) {
+    let htmlMessage = '';
+    console.log(sortThis(logs));
+}
+
+function sortThis(logs) {
+    let message = '';
+    let bugfixes = '';
+    let features = '';
+    let others = '';
+
+    logs.map((item) => {
+        if (item.indexOf('bugfix') > -1) {
+            bugfixes += `> ${item.trim().replace(/(LIQ-\d+|LQ-\d+|INTL-\d+)/g, `<a href="https://tripactions.atlassian.net/browse/$&" target="_blank">$&</a>`)}\n`;
+        } else if (item.indexOf('feature') > -1) {
+            features += `> ${item.trim().replace(/(LIQ-\d+|LQ-\d+|INTL-\d+)/g, `<a href="https://tripactions.atlassian.net/browse/$&" target="_blank">$&</a>`)}\n`;
+        } else {
+            others += `> ${item.trim().replace(/(LIQ-\d+|LQ-\d+|INTL-\d+)/g, `<a href="https://tripactions.atlassian.net/browse/$&" target="_blank">$&</a>`)}\n`;
+        }
+    })
+
+    message = `
+    Features: \n
+        ${features} \n
+	BugFixes: \n
+	    ${bugfixes} \n
+	Others: \n
+	    ${others}
+  `;
+    return message;
 }
 
 onInit();
